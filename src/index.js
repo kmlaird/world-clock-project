8018c9df9baf5c1c3e97451c767d4aee9eea310d
kmlaird <kmlaird15@gmail.com>
2023-04-06 15:54:18 -0700

    Current location functionality added

--- src/index.js
+++ src/index.js
@@ -33,6 +33,9 @@ function updateTime() {
 function updateCity(event) {
   function updateNewTime() {
     let cityTimeZone = event.target.value;
+    if (cityTimeZone === `current`) {
+      cityTimeZone = moment.tz.guess();
+    }
     let cityName = cityTimeZone.replace(`_`, ` `).split(`/`)[1];
     let cityDateTime = moment().tz(cityTimeZone);
 
